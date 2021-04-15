6c775047da7fa7e3bee3b96f56c345a4f3665769
jessilin <jessilinlugo@gmail.com>
2021-04-15 15:53:24 -0700

    book results are displaying

---
+++ client/src/components/Book.js
@@ -0,0 +1,14 @@
+import React from "react";
+function book(props){
+    return (
+        <div className="card">
+  <div className="card-body">
+    <h5 className="card-title">{props.title}</h5>
+    <h6 className="card-subtitle">{props.author}</h6>
+    <p className="card-text">{props.summary}</p>
+  </div>
+</div>
+    )
+}
+
+export default book;
--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,9 +1,11 @@
 import React from "react";
 import API from "../utils/API";
+import Book from "./Book";
 
 class Search extends React.Component {
     state = {
-        input: ""
+        input: "",
+        bookData: []
     }
 
     searchBtn = () => {
@@ -11,7 +13,17 @@ class Search extends React.Component {
         API.getGoogleBooks(this.state.input)
         .then(books => {
             console.log(books);
-        })
+            const results = books.data.items;
+            var booksList = results.map(book => ({
+                title: book.volumeInfo.title,
+                summary: book.volumeInfo.description,
+                author: book.volumeInfo.authors[0]})
+            );
+            console.log(booksList);
+            this.setState({
+              bookData: booksList  
+            })
+            })
     }
 
     handleInputChange = (event) => {
@@ -30,7 +42,10 @@ class Search extends React.Component {
                         <input type="text" value={this.state.input} className="form-control" onChange={this.handleInputChange} placeholder="Enter a Book Title here" />
                         <button onClick={this.searchBtn} type="submit" className="btn btn-primary">Submit</button>
                     </div>
-
+            {this.state.bookData.map((book,key) => (<Book title={book.title}
+            key={key}
+            author={book.author}
+            summary={book.summary}/>) )}
                         
 
             </div>
